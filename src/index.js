f40510e69ee640df9dfb2e3a198a606e8398de27
Ricardo Mirón <r.mirontorres@ugto.mx>
2018-11-27 04:09:19 +0100

    Add comments and structure

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+//Import libraries
 'use strict';
 const fs = require('fs');
 const _ = require('lodash');
@@ -9,22 +10,28 @@ const fileName = 'contacts.txt';
 
 let contacts;
 let headers = ['Name', 'Lastname', 'Nickname', 'Phone', 'Email', 'Birthdate'];
-let searchKey = 'Carolina';
 
-var options = ['List all contacts', 'Create contact', 'Delete contact', 'Search contact'];
-var index = readlineSync.keyInSelect(options, 'What do you want to do? ', {guide: false});
+//Menu options
+var options = ['Add contact', 'Remove contact', 'Update contact', 'Contact list', 'Search for a contact'];
+var index = readlineSync.keyInSelect(options, 'What do you want to do? \n', {guide: false});
 
 initialize()
   .then(() => {
     index++;
     switch (index) {
       case 1:
-        listContacts();
-        break;
-      case 2:
         createContact();
         break;
+      case 2:
+        removeContact();
+        break;
       case 3:
+        updateContacts();
+        break;
+      case 4:
+        listContacts();
+        break;
+      case 5:
         let searchIn = ['Name (firstname + lastname)', 'Nickname', 'Phone', 'Email'];
         let choosen = readlineSync.keyInSelect(searchIn, null, {guide: false, cancel: null});
 
@@ -37,6 +44,10 @@ initialize()
     }
   });
 
+
+/* INITIALIZE PROGRAMM
+Reads a text file and maps its element in an object array
+*/
 function initialize() {
 
   return new Promise(function (resolve, reject) {
@@ -65,13 +76,10 @@ function initialize() {
   });
 }
 
-function listContacts() {
-  console.table(contacts);
-  /*console.log(_.map(contacts, (c, i) => {
-    return (i + 1) + '.\t' + _.values(c).join("\t\t| ")
-  }).join('\n'));*/
-}
-
+/* 1. ADD CONTACTS
+Insert new contact information: first name, last name, phone numbers,
+email addresses, nickname and birth date
+*/
 function createContact() {
   askContacts.askQuestions(_.map(headers, (h) => {
     return h + ': '
@@ -85,9 +93,40 @@ function createContact() {
     });
 }
 
+/* 2. REMOVE CONTACT
+Insert new contact information: first name, last name, phone numbers,
+email addresses, nickname and birth date
+*/
+function removeContact() {
+
+}
+
+/* 3. UPDATE CONTACT
+Insert new contact information: first name, last name, phone numbers,
+email addresses, nickname and birth date
+*/
+function updateContacts() {
+
+}
+
+/* 4. CONTACT LIST
+List all the contacts from an object array
+*/
+function listContacts() {
+  console.table(contacts);
+}
+
+/* 5. SEARCH CONTACT
+List all the contacts matching a specified value by first name and last name,
+nickname, phone numbers or email addresses
+*/
 function searchContact(property, value) {
   let c = {};
   c[property] = value;
   console.log(_.some(contacts, {name: 'Carolina'}));
   console.log(_.filter(contacts, c));
+}
+
+function readFile() {
+  console.table(contacts);
 }
